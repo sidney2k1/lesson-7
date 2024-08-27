e748ece73ea7ed872bf430b37099b177c6b4e421
sidney2k1 <sidsingla02@gmail.com>
2024-08-27 17:31:05 +0530

    mainy

---
+++ main3.py
@@ -0,0 +1,10 @@
+mean=38
+wrongnum=36
+correctnum=56
+total=40
+sum=mean*total
+print("the sum of 40 numbers is", sum)
+num2=sum-(wrongnum-correctnum)
+print("sum-(wrongnumber-correctnumber)=",num2)
+mean2=num2/total
+print(mean2)
