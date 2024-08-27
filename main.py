155dc2acc29c349c3f6c5d1f00cfc4ec3319b883
sidney2k1 <sidsingla02@gmail.com>
2024-08-27 17:15:04 +0530

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+v=4
+w=5
+x=8
+y=2
+z=0
+z=(w+v)*x/y
+print("Value of (w+v)*x/y is", z)
+name="alex"
+age=0
+if name=="alex" or name=="john" and age>=2:
+    print("hi")
+else:
+    print("bye bye")
