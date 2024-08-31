8a6718a6b54f4ae64c4e933fb986a12de2bcdcd1
sidney2k1 <sidsingla02@gmail.com>
2024-08-31 12:12:05 +0530

    project

---
+++ project.py
@@ -0,0 +1,7 @@
+a=int(input("Enter your number:"))
+b=int(input("Enter your number:"))
+c=a
+a=b
+b=c
+print("The new value of the first number is ",a)
+print("The new value of the second number is ",b)
